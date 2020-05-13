dfe5b3de4af61b225f6ffb0964a82e163b970d28
Rayn2402 <nicolas.raymon2@usherbrooke.ca>
2020-05-13 12:24:52 -0400

    Logistic regression model added

--- LearningModels.py
+++ LearningModels.py
@@ -17,7 +17,7 @@ learning_rate_choices = ['constant', 'adaptive', 'invscaling']
 
 class LinearModel:
 
-    def __init__(self, phi, M, eta0, learning_rate='constant'):
+    def __init__(self, phi, M, eta0=1, learning_rate='invscaling'):
 
         """
         Linear Machine Learning Models
@@ -156,7 +156,7 @@ class LinearModel:
 
 class SGDRegressor(LinearModel):
 
-    def __init__(self, phi, M, eta0, learning_rate='constant', lamb=0):
+    def __init__(self, phi, M, eta0=1, learning_rate='invscaling', lamb=0):
 
         """
         SGD Linear Regression Machine Learning Models
@@ -211,6 +211,68 @@ class SGDRegressor(LinearModel):
 
         else:
             return np.dot(loss.transpose(), loss)
+
+
+class SGDLogisticRegressor(LinearModel):
+
+    def __init__(self, phi, M, eta0=1, learning_rate='invscaling'):
+        """
+        SGD Linear Regression Machine Learning Models
+
+        :param phi: basis function applied to training data
+        :param M: dimension of the feature vector returned by phi (including bias)
+        :param eta0: learning rate initial value in the training
+        :param learning_rate: learning rate schedule
+        """
+
+        super().__init__(phi, M, eta0, learning_rate=learning_rate)
+
+    def update_w(self, x_n, t_n):
+
+        """
+        Weight update in SGD
+
+        :param x_n: n-th feature vector of our training data set (1 X L numpy array)
+        :param t_n: label associated to the n-th feature vector (1 x 1 numpy array)
+        :return: updated weight vector
+        """
+        self.w = self.w + self.eta*(self.predict(x_n) - t_n[0][0])*self.phi(x_n).transpose()
+
+    def predict(self, x):
+
+        """
+        Predict the probability for an input x to have label 1
+
+        :param x: 1 x M numpy array
+        :return: predicted probability
+        """
+        return 1 / (1 + np.exp(np.dot(self.phi(x), self.w)[0][0]))
+
+    def loss(self, X, t, return_predictions=False):
+
+        """
+        Computes the cross-entropy loss associated with our current model
+
+        :param X: N x L numpy array with training data (L --> original number of feature)
+        :param t: N x 1 numpy array with training labels
+        :param return_predictions: bool that indicates if we returned the numpy array of predictions
+        :return: float
+        """
+
+        errors = []
+
+        for n in range(X.shape[0]):
+
+            t_n = t[n:n+1][0][0]
+            y_n = self.predict(X[n:n+1])
+            error = t_n*np.log(y_n) + (1-t_n)*np.log(1-y_n)
+            errors.append(error)
+
+        errors = np.array(errors)
+
+        return -np.sum(errors)
+
+
 
 
 
