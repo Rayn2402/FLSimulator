0d966abc9d10df85c864f7708146bc7ea4e780de
Rayn2402 <nicolas.raymon2@usherbrooke.ca>
2020-05-15 12:31:59 -0400

    New __init__ file to set FLSimulator as package

---
+++ FLSimulator/__init__.py
@@ -0,0 +1,7 @@
+from DataManagement import OneDimensionalRDG, OneDimensionalLRDG
+
+from LearningModels import GDRegressor, LogisticRegressor
+
+from Network import Node, FederatedNetwork
+
+from Server import CentralServer
